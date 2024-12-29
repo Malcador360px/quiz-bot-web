ddb202d240428f28696e604c4649016c3ae2cf88
Denis Pyshchai <denis.pyschay@gmail.com>
2024-12-29 01:17:11 +0100

    Try changing dialect

--- src/main/java/com/web/quiz_bot/domain/EmailVerificationToken.java
+++ src/main/java/com/web/quiz_bot/domain/EmailVerificationToken.java
@@ -1,7 +1,6 @@
 package com.web.quiz_bot.domain;
 
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.Instant;
