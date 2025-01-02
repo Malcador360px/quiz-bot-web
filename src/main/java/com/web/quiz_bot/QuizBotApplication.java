08498924a0dc6f053b7a21b3d9125bd913d43eeb
Denis Pyshchai <denis.pyschay@gmail.com>
2025-01-02 21:26:35 +0100

    SSL fixed x5

--- src/main/java/com/web/quiz_bot/QuizBotApplication.java
+++ src/main/java/com/web/quiz_bot/QuizBotApplication.java
@@ -1,12 +1,37 @@
 package com.web.quiz_bot;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuizBotApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException {
+		TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+			public void checkClientTrusted(X509Certificate[] certs, String authType) {
+			}
+			public void checkServerTrusted(X509Certificate[] certs, String authType) {
+			}
+		}
+		};
+		SSLContext sc = SSLContext.getInstance("SSL");
+		sc.init(null, trustAllCerts, new java.security.SecureRandom());
+		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		HostnameVerifier allHostsValid = (hostname, session) -> true;
+		HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
 		SpringApplication.run(QuizBotApplication.class, args);
 	}
 
