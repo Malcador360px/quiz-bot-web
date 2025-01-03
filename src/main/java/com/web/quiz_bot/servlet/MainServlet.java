073103008072b7bad36462eb1b9bd6ab552995dc
Denis Pyshchai <denis.pyschay@gmail.com>
2025-01-03 15:30:54 +0100

    Test PWA x3

--- src/main/java/com/web/quiz_bot/servlet/MainServlet.java
+++ src/main/java/com/web/quiz_bot/servlet/MainServlet.java
@@ -2,7 +2,6 @@ package com.web.quiz_bot.servlet;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.Constants;
-import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.server.VaadinServlet;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
@@ -10,10 +9,5 @@ import javax.servlet.annotation.WebServlet;
 
 @WebServlet(urlPatterns = "/*", name = "slot", asyncSupported = true, initParams = {
         @WebInitParam(name = Constants.I18N_PROVIDER, value = "com.web.quiz_bot.localization.LocalizationProvider")})
-@PWA(
-        name="quiz-bot",
-        shortName="quiz-bot",
-        offlinePath = "offline.html"
-)
-public class MainServlet extends VaadinServlet implements AppShellConfigurator {
+public class MainServlet extends VaadinServlet {
 }
