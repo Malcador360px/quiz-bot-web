61199976569cc38a644ac184becb01307ac8b740
Denis Pyshchai <denis.pyschay@gmail.com>
2025-01-03 15:18:28 +0100

    Test PWA

--- src/main/java/com/web/quiz_bot/servlet/MainServlet.java
+++ src/main/java/com/web/quiz_bot/servlet/MainServlet.java
@@ -1,6 +1,7 @@
 package com.web.quiz_bot.servlet;
 
 import com.vaadin.flow.server.Constants;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.server.VaadinServlet;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
@@ -8,5 +9,10 @@ import javax.servlet.annotation.WebServlet;
 
 @WebServlet(urlPatterns = "/*", name = "slot", asyncSupported = true, initParams = {
         @WebInitParam(name = Constants.I18N_PROVIDER, value = "com.web.quiz_bot.localization.LocalizationProvider")})
+@PWA(
+        name="quiz-bot",
+        shortName="quiz-bot",
+        offlinePath = "offline.html"
+)
 public class MainServlet extends VaadinServlet {
 }
--- src/main/java/com/web/quiz_bot/vaadin/view/MainView.java
+++ src/main/java/com/web/quiz_bot/vaadin/view/MainView.java
@@ -2,6 +2,8 @@ package com.web.quiz_bot.vaadin.view;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.InitialPageSettings;
+import com.vaadin.flow.server.PageConfigurator;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.flow.spring.annotation.UIScope;
 import com.web.quiz_bot.vaadin.layout.OuterMenuLayout;
