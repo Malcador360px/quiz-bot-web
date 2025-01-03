75be5603e6d17933d7a6d9f19c35676e39f38998
Denis Pyshchai <denis.pyschay@gmail.com>
2025-01-03 12:47:52 +0100

    Email change

--- src/main/java/com/web/quiz_bot/QuizBotApplication.java
+++ src/main/java/com/web/quiz_bot/QuizBotApplication.java
@@ -2,13 +2,6 @@ package com.web.quiz_bot;
 
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
